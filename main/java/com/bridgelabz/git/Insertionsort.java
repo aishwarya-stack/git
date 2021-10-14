0ccdb8e227b5d461208082059297fd57c3f01bdb
aishwarya-stack <aishwaryaashokkadam@gmail.com>
2021-10-14 14:07:13 +0530

    Add:Insertion sort program added

---
+++ main/java/com/bridgelabz/git/Insertionsort.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.git;
+
+public class Insertionsort {
+	public static <T extends Comparable<T>> void insertionSort(T[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			T temp = arr[i];
+			int j = i;
+			while (j > 0 && arr[j - 1].compareTo(temp) > 0) {
+				arr[j] = arr[j - 1];
+				j--;
+			}
+			arr[j] = temp;
+		}
+	}
+}
